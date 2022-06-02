c35acd7e861c5918a88ef5d71f57e169cbc3ef08
lefotrakrm121711@gmail.com <Aa121711!>
2022-06-02 19:59:57 +0300

    f

--- src/WebServer.java
+++ src/WebServer.java
@@ -1,34 +1,48 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class WebServer {
     private static BufferedReader in;
     private static BufferedWriter out;
     private static ServerSocket serv;
     private static Socket clientSocket;
+
     public static void main(String[] args) throws IOException {
-        while(true){
+        while (true) {
             try {
-                serv = new ServerSocket(23);
+                serv = new ServerSocket(1234);
                 System.out.println("Сервер запущен");
                 clientSocket = serv.accept();
-                try {
-                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-                    String mess = in.readLine();
-                    System.out.println(mess);
-                    out.write("Вы написали"+mess+"\n");
-                    out.flush();
-                } finally {
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+            } catch (Exception r) {
+            }
+            Scanner sc = new Scanner(System.in);
+
+            try {
+                String mess = in.readLine();
+                if (mess.equals("bye")) {
                     clientSocket.close();
                     in.close();
                     out.close();
+                    System.out.println("Сервер закрыт");
+                    serv.close();
+                    System.exit(0);
                 }
-            } finally {
-                System.out.println("Сервер закрыт");
-                serv.close();
+
+
+                System.out.println(mess);
+                String mess2 = sc.nextLine();
+                out.write(mess2 + "\n");
+                out.flush();
+            } catch (Exception e) {
             }
+
+
+
+
         }
 
     }
